4749115600e50f359714a8fcce14ad5dd9dc1af2
learn-git <alaminhossen176466@gmail.com>
2025-11-25 09:06:40 +0600

    fead: implement getLength function

--- solution.ts
+++ solution.ts
@@ -16,3 +16,17 @@ return !value
     
 return value
 }
+
+
+function getLength(value : string | any[]) : number  {
+    
+if(typeof value === 'string'){
+    return value.length
+}
+
+if(Array.isArray(value)){
+    return value.length
+}
+
+  return  0
+}
