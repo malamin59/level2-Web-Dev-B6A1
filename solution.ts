1b8257160afe4c7f21772dbf36073956ba506244
learn-git <alaminhossen176466@gmail.com>
2025-11-25 08:49:32 +0600

    feat: add formatValue function to handle to string number & boolean

---
+++ solution.ts
@@ -0,0 +1,18 @@
+
+type Values = string | number | boolean
+function formatValue (value: Values) : string | number | boolean {
+
+if(typeof value === "string") {
+    return value.toUpperCase()
+}
+
+if(typeof value === "number") {
+    return value * 10
+}
+
+if(typeof value === 'boolean') {
+return !value
+}
+    
+return value
+}
